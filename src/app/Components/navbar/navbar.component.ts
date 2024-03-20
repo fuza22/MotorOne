d70dd4ae3d8f1ed0c95b665cd5d83fb7a8d14c93
fuza22 <barone.emanuele8@gmail.com>
2024-03-20 17:03:47 +0100

    navbar-mobile

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { AuthService } from '../../Services/auth.service';
-import { IUser } from '../../Models/auth/i-user';
+import * as bootstrap from 'bootstrap';
 import { IAuthData } from '../../Models/auth/i-auth-data';
 
 @Component({
@@ -8,33 +8,30 @@ import { IAuthData } from '../../Models/auth/i-auth-data';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
 
-constructor(
+  constructor(private authService: AuthService) {}
 
-  private authService: AuthService
+  hide!: boolean;
+  isLoggedIn$!: boolean;
+  myUser!: IAuthData;
 
-){}
 
-hide!:boolean;
-isLoggedIn$!: boolean;
-myUser!: IAuthData;
+  ngOnInit() {
+    this.authService.user$.subscribe(res => {
+      if (res) this.myUser = res;
+    });
+    this.authService.isLoggedIn$.subscribe(res => this.isLoggedIn$ = res);
+  }
 
-ngOnInit(){
+  logout() {
+    this.authService.logout();
+  }
 
-  this.authService.user$.subscribe(res => {
-
-    if(res) this.myUser = res;
-
-  })
-  this.authService.isLoggedIn$.subscribe(res => this.isLoggedIn$ = res);
-
-}
-
-logout(){
-
-  this.authService.logout();
-
-}
-
+  ngAfterViewInit() {
+    var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'));
+    offcanvasElementList.map(function(offcanvasEl) {
+      return new bootstrap.Offcanvas(offcanvasEl);
+    });
+  }
 }
