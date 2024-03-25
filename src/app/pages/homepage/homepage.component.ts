b87379a43d3370d16e99445f999b266a477900f0
fuza22 <barone.emanuele8@gmail.com>
2024-03-25 16:45:44 +0100

    navbar fix

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -15,21 +15,21 @@ export class HomepageComponent {
 
   constructor(authSvc: AuthService) {}
 
-  ngOnInit(){
+  //ngOnInit(){
+//
+  //  this.loggedIn();
+//
+  //}
 
-    this.loggedIn();
-
-  }
-
-  loggedIn(){
-
-   const user:string|null = localStorage.getItem('accessData');
-
-    if(!user) return alert("Not logged in")
-
-    const data: IAuthData = JSON.parse(user)
-    this.myUser = data.user;
-
-  }
+  //loggedIn(){
+//
+  // const user:string|null = localStorage.getItem('accessData');
+//
+  //  if(!user) return alert("Not logged in")
+//
+  //  const data: IAuthData = JSON.parse(user)
+  //  this.myUser = data.user;
+//
+  //}
 }
 
