7796ed0351667db601d798d26cb21bc219d6b7fa
fuza22 <barone.emanuele8@gmail.com>
2024-03-27 16:50:17 +0100

    navbar fix

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, HostListener } from '@angular/core';
 import { AuthService } from '../../Services/auth.service';
 import * as bootstrap from 'bootstrap';
 import { IAuthData } from '../../Models/auth/i-auth-data';
@@ -34,4 +34,12 @@ export class NavbarComponent implements AfterViewInit {
       return new bootstrap.Offcanvas(offcanvasEl);
     });
   }
+
+  isScrolled = false;
+
+    @HostListener("window:scroll")
+    scrollEvent() {
+    window.scrollY >= 80 ? (this.isScrolled = true) : (this.isScrolled = false);
+}
+
 }
